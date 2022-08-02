074cbc09c40d083d16a9ac2f6485465fa2f0e196
Jorge G <jorgeald77@gmail.com>
2022-08-01 21:27:33 -0500

    Se realiza la consulta a API

--- js/main.js
+++ js/main.js
@@ -1,4 +1,46 @@
-console.log("CODERHOUSE - Entregable 08: Optimizando el proyecto final");
+console.log("CODERHOUSE - Entregable 10: Fetch en tu proyecto");
+
+// Elementos del DOM
+const containerUsuario = document.getElementById("container-usuario");
+const formUsuario = document.getElementById("form-usuario");
+
+const containerApp = document.getElementById("container-app");
+const tagBievendia = document.getElementById("tag-bienvenido");
+
+const tagMisVotos = document.getElementById("tag-misvotos");
+const tagItemsVotos = document.getElementById("tag-items-votos");
+
+const elementGeneros = document.getElementById("generos");
+const elementPeliculas = document.getElementById("peliculas");
+const elementNumVotos = document.getElementById("numvotos");
+
+const genres = JSON.parse(sessionStorage.getItem('genres'));
+
+if(sessionStorage.getItem('genres') === null) {
+  fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=46870299310130ffbfbd57d5fd9e6951&language=es')
+  .then((response) => response.json())
+  .then((data) => {
+    sessionStorage.setItem('genres', JSON.stringify(data.genres))
+  })
+}
+
+
+function getPelisByGenere(generoid) {
+  if(sessionStorage.getItem(generoid) === null) {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=46870299310130ffbfbd57d5fd9e6951&with_genres=${generoid}&language=es`)
+    .then((response) => response.json())
+    .then((data) => {
+      sessionStorage.setItem(generoid, JSON.stringify(data.results))
+    })
+  }
+}
+
+
+
+
+
+
+
 
 // Guardar en LocalStorage los catálogos de Películas y Géneros
 localStorage.setItem(
@@ -30,20 +72,6 @@ localStorage.setItem(
   ])
 );
 
-// Elementos del DOM
-const containerUsuario = document.getElementById("container-usuario");
-const formUsuario = document.getElementById("form-usuario");
-
-const containerApp = document.getElementById("container-app");
-const tagBievendia = document.getElementById("tag-bienvenido");
-
-const tagMisVotos = document.getElementById("tag-misvotos");
-const tagItemsVotos = document.getElementById("tag-items-votos");
-
-const elementGeneros = document.getElementById("generos");
-const elementPeliculas = document.getElementById("peliculas");
-const elementNumVotos = document.getElementById("numvotos");
-
 // Obtener datos de LocalStorage
 const generos = JSON.parse(localStorage.generos);
 const peliculas = JSON.parse(localStorage.peliculas);
@@ -74,11 +102,11 @@ class Votos {
 
   // Agregar la pelicula por la que el usuario voto, no permitir duplicados
   agregarVoto(peli) {
-    if (this.votos.some((el) => el.cod == peli.cod) == false)
+    if (this.votos.some((el) => el.id == peli.id) == false)
       this.votos.push(peli);
 
       Toastify({
-        text: `Votaste por: ${peli.titulo}`,
+        text: `Votaste por: ${peli.title}`,
         duration: 3000, 
         gravity: 'top',
         position: 'right',
@@ -91,16 +119,16 @@ class Votos {
 
   // Eliminar pelicula de la votación
   borrarVoto(code) {
-    let peli = this.votos.find((el) => el.cod == code)
+    let peli = this.votos.find((el) => el.id == code)
 
-    if (this.votos.some((el) => el.cod === code)) {
+    if (this.votos.some((el) => el.id === code)) {
       this.votos.splice(
-        this.votos.findIndex((el) => el.cod === code),
+        this.votos.findIndex((el) => el.id === code),
         1
       );
 
       Toastify({
-        text: `Eliminaste: ${peli.titulo}`,
+        text: `Eliminaste: ${peli.title}`,
         duration: 3000, 
         gravity: 'top',
         position: 'right',
@@ -119,31 +147,43 @@ class Votos {
     elementNumVotos.innerText = this.contarVotos();
     tagItemsVotos.innerHTML = "";
     this.votos.forEach((voto) => {
-      tagItemsVotos.innerHTML += `<li class='flex justify-start'><button class='mr-1' onclick='votos.borrarVoto("${voto.cod}")'><svg xmlns='http://www.w3.org/2000/svg' class='h-4 w-4 text-red-500' viewBox='0 0 20 20' fill='currentColor'> <path fill-rule='evenodd' d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z' clip-rule='evenodd' /></svg></button><p>${voto.titulo}</p></li>`;
+      tagItemsVotos.innerHTML += `<li class='flex justify-start'><button class='mr-1' onclick='votos.borrarVoto("${voto.id}")'><svg xmlns='http://www.w3.org/2000/svg' class='h-4 w-4 text-red-500' viewBox='0 0 20 20' fill='currentColor'> <path fill-rule='evenodd' d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z' clip-rule='evenodd' /></svg></button><p>${voto.title}</p></li>`;
     });
   }
 }
 const votos = new Votos();
 
+
 // Llenar elemento Select con los géneros de películas
+/*
 generos.forEach((value, index, array) => {
   elementGeneros.innerHTML += `<option value="${value}"><span class="uppercase">${value}</span></option>`;
 });
+*/
+genres.map(function (g) {
+  elementGeneros.innerHTML += `<option value="${g.id}"><span class="uppercase">${g.name}</span></option>`;
+})
+
 
 // Función para construir menu de peliculas segun el género elegido.
 function filtrarPeliculasPorGenero(generoElegido) {
+  this.getPelisByGenere(generoElegido)
+
   // Dejar variables en blanco o vacias para construir un menu nuevo.
-  peliculasFiltradas = [];
+  // peliculasFiltradas = [];
 
   // Hacer uso del filter() para obtener las películas segun el genereElegido
-  peliculasFiltradas = peliculas.filter((el) =>
+  /*peliculasFiltradas = peliculas.filter((el) =>
     el.genero.includes(generoElegido)
-  );
+  );*/
 
   // Construir menu películas a partir de recorrer el array "peliculasFiltradas" para mostrarlo en el Prompt.
-  peliculasFiltradas.forEach((value, index, array) => {
+  /*peliculasFiltradas.forEach((value, index, array) => {
     elementPeliculas.innerHTML += `<button role="button" class="btn-pelicula" aria-cod="${value.cod}">${value.titulo}</button>`;
-  });
+  });*/
+  JSON.parse(sessionStorage.getItem(generoElegido)).map(function (mov) {
+    elementPeliculas.innerHTML += `<button role="button" class="btn-pelicula" aria-gen="${generoElegido}" aria-cod="${mov.id}">${mov.title}</button>`;
+  })
 }
 
 // Listener submit, formulario
@@ -167,6 +207,6 @@ elementGeneros.addEventListener("change", (e) => {
 // Listener click, peliculas
 elementPeliculas.addEventListener("click", (e) => {
   votos.agregarVoto(
-    peliculasFiltradas.find((el) => el.cod == e.target.getAttribute("aria-cod"))
+    JSON.parse(sessionStorage.getItem(e.target.getAttribute("aria-gen"))).find((el) => el.id == e.target.getAttribute("aria-cod"))
   );
 });
