3168c99c91a76c166ba54fddd73bc6f66d8f9e1c
Jorge G <jorgeald77@gmail.com>
2022-08-13 10:59:11 -0500

    Se actualizo el frameworkcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./js/*.js"],
-  theme: {
-    extend: {
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'appgray': '#C2D9E8',
-        'appblue': '#60B2F0',
-        'appgreen': '#027D8D',
-        'appred': '#F25D50',
-        'apporange': '#FFA652',
-      },
+    content: ["./*.html", "./js/*.js"],
+    theme: {
+        extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'appgray': '#C2D9E8',
+                'appblue': '#60B2F0',
+                'appgreen': '#027D8D',
+                'appred': '#F25D50',
+                'apporange': '#FFA652',
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
 }
